ab57a203070e89d20ee8f23b86bb5a88e1ab2a37
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-03-13 22:49:07 +0200

    feat: add basic win/lose condition based on C4 note

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from midi_utils import note_on, note_off
+from music import note_val
 import threading
 import time
 
@@ -11,12 +12,15 @@ def handle_midi_message(message, output_port):
             note_thread = threading.Thread(target=lambda: note_on(output_port, message.note, message.velocity))
             note_thread.daemon = True  # Thread will exit when main program exits
             note_thread.start()
+            return message.note
     elif message.type == 'note_off':
         # When a note is released, send note off
         print(f"Received note off: {message.note}")
         note_thread = threading.Thread(target=lambda: note_off(output_port, message.note, message.velocity))
         note_thread.daemon = True
         note_thread.start()
+
+    return False
 
 def game_loop(input_port, output_port):
     print("Game loop started. Press keys on your MIDI device...")
@@ -25,6 +29,11 @@ def game_loop(input_port, output_port):
     try:
         # Set up callback for incoming MIDI messages
         for message in input_port:
-            handle_midi_message(message, output_port)
+            played_note = handle_midi_message(message, output_port)
+            if played_note:
+                if played_note == note_val('C4'):
+                    print("\nWIN")
+                else:
+                    print("\nLOSE")
     except KeyboardInterrupt:
         print("\nGame loop stopped by user")
