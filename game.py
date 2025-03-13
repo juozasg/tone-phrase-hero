38a263656eac23654e15a57cfc03599d5de224d0
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-03-13 22:12:31 +0200

    refactor: update game_loop to prepare for input port reading

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 from midi_utils import play_note
 
 def game_loop(input_port, output_port):
-    if input_port and output_port:
-        # Play C4 note for 1 second
-        play_note(output_port, note=60, velocity=64, duration=1.0)
+    # Play C4 note for 1 second
+    # read note from input_port and play it AI!
+    play_note(output_port, note=60, velocity=64, duration=1.0)
