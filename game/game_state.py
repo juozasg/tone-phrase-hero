d3bf8c63b497e2cf55d8ae0d6e4e59c77b9d04d0
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-03-14 21:59:03 +0200

    good puzzle generation

--- game/game_state.py
+++ game/game_state.py
@@ -1,33 +1,62 @@
-from game.generate_challenge import generate_challenge
+from game.generate_challenge import MelodyShape, generate_challenge, make_note_options
 from game.notify import play_challenge
 
 import random
+# from enum import Enum
 
 class GameState:
     def __init__(self):
         self.current_position = 0
         self.target_sequence = []
-        self.note_options = ['C4', 'D4']
+        # self.note_options = ['C4', 'D4']
 
-        self.notes_difficulty_tiers: list[list[str]] = [
-            ['A3', 'B3', 'E4'], # 1, M2, P5
-            ['Bb3'], # m2
-            ['C4'], # m3
-            ['Db4'], # M3
-            ['D4'], # P4
-            ['Eb4'], # TT
-            ['F4'], # m6
-            ['Gb4'], # M6
-            ['G4'], # m7
-            ['Ab4'], # M7
-        ]
+        self.note_difficulty: int = 0 # 0 to 9
+        self.shape_difficulty: int = 0 # 0 to 3
+        self.leapiness_difficulty: float = 0.1 # 0 to 1
+
+        self.consecutive_successes = 0
+        self.consecutive_failures = 0
+
+        self.total_successes = 0
+        self.total_failures = 0
+
+        self.length_difficulty = 0
+
+
+    def update_difficulty(self):
+        # if more wins than loses, roll 25% to increase note_difficulty. with more consecutive wins, increase chance
+        if(self.total_successes > self.total_failures ):
+            if(random.random() < 0.25 + (self.consecutive_successes * 0.05)):
+                self.note_difficulty = min(9, self.note_difficulty + 1)
+        # if more loses than wins, roll 50% to decrease note_difficulty
+        else:
+            if(random.random() < 0.50):
+                self.note_difficulty = max(0, self.note_difficulty - 1)
+        print(f'note_difficulty: {self.note_difficulty}')
+
+       # every 3 successes or any failures roll 25% to increase or decrease shape_difficulty
+        if(self.consecutive_successes % 3 == 0 and random.random() > 0.75):
+            self.shape_difficulty = min(3, self.shape_difficulty + 1)
+        elif(self.consecutive_failures > 0 and random.random() > 0.75):
+            self.shape_difficulty = max(0, self.shape_difficulty - 1)
+
+
+        print(f'shape_difficulty: {self.shape_difficulty}')
+
+        print("totals: ", self.total_successes, self.total_failures, "consequetive totals: ", self.consecutive_successes, self.consecutive_failures)
+
 
     def new_challenge(self):
         # Randomize sequence length from 2 to 4
-        sequence_length = random.randint(2, 4)
+        sequence_length = random.randint(1, self.length_difficulty + 1)
 
-        # Generate a new sequence of notes to guess
-        self.target_sequence = generate_challenge(sequence_length, self.note_options)
+        note_options = make_note_options(self.note_difficulty)
+
+        # Convert numeric shape_difficulty to MelodyShape enum
+        shape = MelodyShape(self.shape_difficulty)
+        leapiness = ((self.length_difficulty + self.note_difficulty)/10.0) * self.leapiness_difficulty
+
+        self.target_sequence = [note_options[0]] + generate_challenge(sequence_length, note_options, shape, leapiness)
         self.current_position = 0
 
         print("\n=== NEW CHALLENGE ===")
@@ -47,7 +76,27 @@ class GameState:
         return self.target_sequence[self.current_position]
 
     def success(self):
-        pass
+        self.consecutive_successes += 1
+        self.consecutive_failures = 0
+        self.leapiness_difficulty = min(1, self.leapiness_difficulty + 0.15)
+        if(random.random() < 0.30):
+            self.length_difficulty += 1
+            if(self.length_difficulty > 8):
+                self.length_difficulty = 8
+
+        print(f'leapiness_difficulty: {self.leapiness_difficulty}. length_difficulty: {self.length_difficulty}')
+        self.total_successes += 1
+        self.update_difficulty()
+
 
     def failure(self):
-        pass
+        self.consecutive_failures += 1
+        self.consecutive_successes = 0
+        self.leapiness_difficulty = max(0, self.leapiness_difficulty - 0.15)
+        if(random.random() < 0.60):
+            self.length_difficulty -= 1
+            if(self.length_difficulty < 1):
+                self.length_difficulty = 1
+        print(f'leapiness_difficulty: {self.leapiness_difficulty}. length_difficulty: {self.length_difficulty}')
+        self.total_failures += 1
+        self.update_difficulty()
--- game/generate_challenge.py
+++ game/generate_challenge.py
@@ -1,23 +1,109 @@
 import random
+import math
 from enum import Enum
 
+from game.music import note_val
+
+
+def make_note_options(difficulty_tier: int) -> list[str]:
+    notes_difficulty_tiers: list[list[str]] = [
+        ['A3', 'B3', 'E4'], # 1, M2, P5
+        ['Bb3'], # m2
+        ['C4'], # m3
+        ['Db4'], # M3
+        ['D4'], # P4
+        ['Eb4'], # TT
+        ['F4'], # m6
+        ['Gb4'], # M6
+        ['G4'], # m7
+        ['Ab4'], # M7
+    ]
+
+    # merge all previous tiers
+    note_options = []
+    for i in range(difficulty_tier + 1):
+        note_options += notes_difficulty_tiers[i]
+
+    # sort notes by note_val
+    note_options.sort(key=lambda x: note_val(x))
+    return note_options
+
+
 class MelodyShape(Enum):
-    UP = 1
-    DOWN = 2
-    UPDOWN = 3
-    RANDOM = 4
+    UP = 0 # start at note_options[0]. each note is higher than the previous or the same
+    DOWN = 1 # start at last note in note_options. each note is lower than the previous or the same
+    UPDOWN = 2 # start at note_options[0]. each note is higher than the previous or the same until halfway, then each note is lower than the previous or the same
+    RANDOM = 3 # random order of notes
 
 def generate_challenge(sequence_length: int, note_options: list[str], shape: MelodyShape = MelodyShape.UP, leapiness: float = 0.2) -> list[str]:
+    # print(f'Generating challenge with shape {shape} and leapiness {leapiness}. Notes = {note_options}. LEN = {sequence_length}')
     target_sequence: list[str] = []
 
-    # Always start with the first note from note_options
-    if note_options and sequence_length > 0:
-        target_sequence.append(note_options[0])
-    
-    # Generate the rest of the sequence
-    for i in range(1, sequence_length):
-        # Choose a random note
-        target_note = random.choice(note_options)
-        target_sequence.append(target_note)
+    if shape == MelodyShape.UP:
+        # leapiness = leapiness * 0.5
+        idx = 0
+        force_step = False
+        for _ in range(sequence_length):
+            leap = random.random() < leapiness
+            leapiness = leapiness * 0.75 if leap == True else leapiness * 1.15
+
+            if leap:
+                idx = random.randint(idx, len(note_options) - 1)
+                target_sequence.append(note_options[idx])
+            else:
+                # step or stay
+                step = random.randint(0, 1)
+                if step == 0:
+                    if force_step == True:
+                        idx = idx + 1
+                        force_step = False
+                    else:
+                        force_step = True
+                idx = idx + step
+                if idx > len(note_options) - 1:
+                    idx = len(note_options) - 1
+
+                target_sequence.append(note_options[idx])
+
+    elif shape == MelodyShape.DOWN:
+        reversed_note_options = note_options[::-1]
+        target_sequence = generate_challenge(sequence_length, reversed_note_options, MelodyShape.UP, leapiness)
+
+    elif shape == MelodyShape.UPDOWN:
+        half_length = sequence_length // 2
+        up = generate_challenge(half_length, note_options, MelodyShape.UP, leapiness * half_length)
+        # Get the last note from the up sequence and its index in note_options
+        last_up_note = up[-1]
+        max_idx = min(note_options.index(last_up_note) + 1, len(note_options) - 1)
+
+        # slice note options to remove notes that are not in the up sequence
+        down_note_options = list(reversed(note_options[:max_idx]))
+        # remove note otions that are not in the up sequence and one more note
+        down = generate_challenge(half_length, down_note_options, MelodyShape.UP, leapiness)
+        target_sequence = up + [note_options[max_idx]] + down
+
+    elif shape == MelodyShape.RANDOM:
+        leapiness = leapiness * 1.5
+
+        # Start with a random note
+        target_sequence = [random.choice(note_options)]
+        for _ in range(sequence_length - 1):
+            current_index = note_options.index(target_sequence[-1])
+            if random.random() < leapiness:  # Make a leap. 300% more likely to leap in random shape
+                # For leaps, choose a note that's at least 2 steps away in either direction
+                possible_indices = [i for i in range(len(note_options)) if abs(i - current_index) >= 2]
+                if not possible_indices:  # If no leap is possible
+                    next_index = random.randint(0, len(note_options) - 1)
+                else:
+                    next_index = random.choice(possible_indices)
+            else:  # No leap, choose an adjacent note or stay the same
+                # For non-leaps, choose a note that's at most 1 step away in either direction
+                lower_bound = max(0, current_index - 1)
+                upper_bound = min(len(note_options) - 1, current_index + 1)
+                next_index = random.randint(lower_bound, upper_bound)
+            target_sequence.append(note_options[next_index])
 
     return target_sequence
+
+
+
