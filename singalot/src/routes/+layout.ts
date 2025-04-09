34aa0d87f2ca51c6fa27ef0b7b0e93e164f7a488
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-04-09 20:54:47 +0300

    DO friendly path

--- singalot/src/routes/+layout.ts
+++ singalot/src/routes/+layout.ts
@@ -1,5 +1,7 @@
 export const ssr = false;
 export const prerender = true;
+export const trailingSlash = 'always';
+
 
 // export function load() {
 // 	const scores = Array.from({ length: 22 }, () => Math.floor(Math.random() * 191) + 10);
