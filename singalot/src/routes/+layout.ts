f191d97162dbadecec81fab9599ac0c376c3a17c
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-04-24 15:51:46 +0300

    audio files

--- singalot/src/routes/+layout.ts
+++ singalot/src/routes/+layout.ts
@@ -3,12 +3,12 @@ export const prerender = true;
 export const trailingSlash = 'always';
 
 import '$lib/audio.svelte';
-import { loadAudio } from '$lib/audio.svelte';
+import { loadAudioData } from '$lib/audio.svelte';
 // import { SplendidGrandPiano, Reverb } from "smplr";
 
 export async function load() {
 	if (typeof AudioContext !== 'undefined') {
-		loadAudio();
+		loadAudioData();
 
 	}
 }
